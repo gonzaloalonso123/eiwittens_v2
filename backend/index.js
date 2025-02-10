7f4cd896f261135dafd3eb185a4366d3a5ba53d2
Gonzalo  Alonso <gonzaloalonso@Gonzalos-MacBook-Air.local>
2025-02-10 18:59:35 +0100

    extra

--- backend/index.js
+++ backend/index.js
@@ -51,7 +51,7 @@ app.get("/posts", async (req, res) => {
 
 app.post("/product-clicked/:id", async (req, res) => {
   const id = req.params.id;
-  const extra = req.query.extra;
+  const extra = req.query.extra || null
   await addClickedTimeToProduct(id, extra);
   res.status(200).send("ok");
 });
