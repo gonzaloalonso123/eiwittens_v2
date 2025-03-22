84eb0a34e336c05ab1915128fd25db7650e0d9b4
Gonzalo  Alonso <gonzaloalonso@Gonzalos-MacBook-Air.local>
2025-03-22 16:30:13 +0100

    pls

--- backend/database/firebase.js
+++ backend/database/firebase.js
@@ -1,9 +1,8 @@
 const admin = require("firebase-admin");
-const dotenv = require("dotenv");
 
 let serviceAccount = require("../.firebase/service_account.json");
 admin.initializeApp({
-  credential: serviceAccount,
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
